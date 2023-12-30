d00125c88d7a4b39e0ae421a588981aa16b2a445
vizn3r <77296854+vizn3r@users.noreply.github.com>
2023-12-30 02:26:10 +0100

    test

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,11 @@
 return {
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" }
+  }
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
